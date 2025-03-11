5265212f3ef069047b1deda15fa3d32d154792c8
Taosif-Tabib <taosiftabib659@gmail.com>
2025-03-11 20:14:41 +0600

    Add files via upload

---
+++ Java array problem/print2darray/Print2DArray.java
@@ -0,0 +1,15 @@
+
+package print2darray;
+
+public class Print2DArray {
+    public static void main(String[] args) {
+        int[][] array = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+        for (int[] row : array) {
+            for (int element : row) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+    }
+}
